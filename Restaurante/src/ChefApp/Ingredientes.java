f08dbd44be55b8b2afb07e8023b1b1abbcdab767
Maryxd <mary1300@live.com>
2016-11-03 20:26:06 -0600

    Arreglo de Ingredientes

--- Restaurante/src/ChefApp/Ingredientes.java
+++ Restaurante/src/ChefApp/Ingredientes.java
@@ -1,44 +1,113 @@
 package ChefApp;
 
-import java.util.ArrayList;
-import java.util.List;
+import Estructuras_Basicas.Lista;
+
+
 
 public class Ingredientes {
-	List <Ingrediente> frutas,vegetales,granos,lacteos,carnes;
+	Lista <Ingrediente> frutas,vegetales,granos,lacteos,carnes;
+	Lista<Ingrediente>todos;
 	
 	
-	
+	public Ingredientes(Lista<Ingrediente> frutas, Lista<Ingrediente> vegetales, Lista<Ingrediente> granos,
+			Lista<Ingrediente> lacteos, Lista<Ingrediente> carnes) {
+		super();
+		this.frutas = frutas;
+		this.vegetales = vegetales;
+		this.granos = granos;
+		this.lacteos = lacteos;
+		this.carnes = carnes;
+	}
+	public Ingredientes(Lista<Ingrediente>todos){
+		this.todos=todos;
+		
+	}
+
+	public Lista<Ingrediente> getFrutas() {
+		return frutas;
+	}
+
+	public void setFrutas(Lista<Ingrediente> frutas) {
+		this.frutas = frutas;
+	}
+
+	public Lista<Ingrediente> getVegetales() {
+		return vegetales;
+	}
+
+	public void setVegetales(Lista<Ingrediente> vegetales) {
+		this.vegetales = vegetales;
+	}
+
+	public Lista<Ingrediente> getGranos() {
+		return granos;
+	}
+
+	public void setGranos(Lista<Ingrediente> granos) {
+		this.granos = granos;
+	}
+
+	public Lista<Ingrediente> getLacteos() {
+		return lacteos;
+	}
+
+	public void setLacteos(Lista<Ingrediente> lacteos) {
+		this.lacteos = lacteos;
+	}
+
+	public Lista<Ingrediente> getCarnes() {
+		return carnes;
+	}
+
+	public void setCarnes(Lista<Ingrediente> carnes) {
+		this.carnes = carnes;
+	}
+
 	public Ingredientes(){
-		frutas=new ArrayList<>();
-		granos=new ArrayList<>();
-		vegetales=new ArrayList<>();
-		lacteos=new ArrayList<>();
-		carnes=new ArrayList<>();
+		frutas=new Lista<>();
+		granos=new Lista<>();
+		vegetales=new Lista<>();
+		lacteos=new Lista<>();
+		carnes=new Lista<>();
 	}
 	
 	public void Registrar(String ingrediente, String categoria){
 		if (categoria=="frutas"){
 			Ingrediente n= new Ingrediente(ingrediente);
-			frutas.add(n);
+			frutas.insertarPrimero(n);
 		}
 		else if(categoria=="granos"){
 			Ingrediente n= new Ingrediente(ingrediente);
-			granos.add(n);
+			granos.insertarPrimero(n);
 		}
 		else if(categoria=="vegetales"){
 			Ingrediente n= new Ingrediente(ingrediente);
-			vegetales.add(n);
+			vegetales.insertarPrimero(n);
 	    }
 		else if(categoria=="lacteos"){
 			Ingrediente n= new Ingrediente(ingrediente);
-			lacteos.add(n);
+			lacteos.insertarPrimero(n);
 	    }
 		else if(categoria=="carnes"){
 			Ingrediente n= new Ingrediente(ingrediente);
-			carnes.add(n);
+			carnes.insertarPrimero(n);
 	    }
 		else{
-			System.err.print("Categoría no válida");
+			System.err.print("Categoria no valida");
 		}
-	}	
+	}
+	
+	public void acomodar_listas(){
+		for (int i=0; i<todos.cuantosElementos(); i++){
+			
+		}
+	}
+	
+	public void ordenar(){
+		Sort.quick(frutas);
+		
+		Sort.shell(vegetales);
+		Sort.bubble(lacteos);
+		Sort.insertion(carnes);
+	}
 }
